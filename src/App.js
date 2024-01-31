301373bf4bf80b8993cf63f4cefe07937a7d2884
Burak Kivrak <m-burak09@hotmail.com>
2024-01-31 16:26:14 +0100

    restapi

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-function App() {
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const url = 'http://localhost/educom/tohabit/server/mobile/list/5/'
+  
+   useEffect(() => {
+      fetch(url)
+         .then((response) => response.json())
+         .then((data) => {
+            console.log(data);
+            setPosts(data);
+         })
+         .catch((err) => {
+            console.log("Fetch error:", err);
+         });
+   }, []);
+
   return (
     <div className="App">
+      <h1>tohabit</h1>
+      
     </div>
   );
 }
+
+
 
 export default App;
