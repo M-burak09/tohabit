92f78092ab9b43426298db54fa5f8cb6cf3f1e1e
Burak Kivrak <m-burak09@hotmail.com>
2024-02-16 11:07:57 +0100

    changed seed bug

--- server/sql/seeds.sql
+++ server/sql/seeds.sql
@@ -10,12 +10,12 @@ INSERT INTO `Task` (`id`, `user_id`, `title`, `description`, `label`) VALUES
 
 INSERT INTO `Todo` (`id`, `task_id`, `image`, `date`, `completion`) VALUES
 (2, 2, 'todo.png', '2024-6-9', 0),
-(3, 3, 'todo.png', '2024-3-2', 0);
+(3, 3, 'todo.png', '2024-6-2', 0);
 
 INSERT INTO `Habit` (`id`, `task_id`, `image`, `start_date`, `day_of_week`) VALUES
 (1, 1, 'habit.png', '2024-1-8', NULL);
 
 INSERT INTO `Habit_instance` (`id`, `habit_id`, `date`, `completion`) VALUES
-(1, 1, '2024-1-8', 0),
-(2, 1, '2024-1-15', 0),
-(3, 1, '2024-1-22', 0);
+(10, 1, '2024-1-8', 0),
+(11, 1, '2024-1-15', 0),
+(12, 1, '2024-1-22', 0);
